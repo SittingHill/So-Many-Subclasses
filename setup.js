ec3734575659e8fa72161f6fe386119613169188
SittingHill <72323705+SittingHill@users.noreply.github.com>
2025-10-02 08:39:52 -0500

    Add files via upload

---
+++ setup.js
@@ -0,0 +1,9 @@
+Hooks.once('init', () => {
+
+//Adds new class feature types
+CONFIG.DND5E.featureTypes.class.subtypes.mythicTale = "Mythic Tale";
+CONFIG.DND5E.featureTypes.class.subtypes.feralSwipe = "Feral Swipe";
+  CONFIG.DND5E.featureTypes.class.subtypes.beastlordsAspect = "Beastlord's Aspect";
+
+
+ });
