66a4d3e8be8deb7b7c030b28b6190ed91a4d8466
SittingHill <72323705+SittingHill@users.noreply.github.com>
2025-10-10 17:56:41 -0500

    Add files via upload

--- setup.js
+++ setup.js
@@ -1,9 +1,18 @@
 Hooks.once('init', () => {
 
-//Adds new class feature types
-CONFIG.DND5E.featureTypes.class.subtypes.mythicTale = "Mythic Tale";
-CONFIG.DND5E.featureTypes.class.subtypes.feralSwipe = "Feral Swipe";
-  CONFIG.DND5E.featureTypes.class.subtypes.beastlordsAspect = "Beastlord's Aspect";
+	//Adds new class feature types
+	CONFIG.DND5E.featureTypes.class.subtypes.mythicTale = "Mythic Tale";
+	CONFIG.DND5E.featureTypes.class.subtypes.feralSwipe = "Feral Swipe";
+	CONFIG.DND5E.featureTypes.class.subtypes.beastlordsAspect = "Beastlord's Aspect";
+	CONFIG.DND5E.featureTypes.class.subtypes.runemark = "Runemark";
+	CONFIG.DND5E.featureTypes.class.subtypes.enhancedRunemark = "Enhanced Runemark";
 
-
+  // Make Psionics a valid spellcasting school
+  CONFIG.DND5E.spellSchools.psi = {
+    label: 'KCCC.SchoolPsi',
+    icon: 'modules/kibbles-compendium-of-craft-and-creation/assets/psionics.svg',
+    fullKey: 'psionic',
+    reference: '',
+  };
  });
+ 
